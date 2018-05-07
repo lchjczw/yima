51b60f00b96760299b95baba8ceffeda8a7f3f5d
lchjczw <lchjczw@users.noreply.github.com>
2018-05-08 02:22:10 +0800

    新增截取验证码的函数

--- yima.go
+++ yima.go
@@ -319,3 +319,17 @@ type MobileOption struct {
 	Mobile    string
 	ExcludeNo string
 }
+
+
+func Between(str, starting, ending string) string {
+	s := strings.Index(str, starting)
+	if s < 0 {
+		return ""
+	}
+	s += len(starting)
+	e := strings.Index(str[s:], ending)
+	if e < 0 {
+		return ""
+	}
+	return str[s : s+e]
+}
